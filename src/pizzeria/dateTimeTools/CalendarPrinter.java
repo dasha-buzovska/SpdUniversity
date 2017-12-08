78dba70f085c724bbba5255e4a5458cbb4d108b5
Daryna Buzovska <buzovskadaryna@gmail.com>
2017-12-08 16:53:00 +0200

    HW9-Time - refactoring of task4

--- src/pizzeria/dateTimeTools/CalendarPrinter.java
+++ src/pizzeria/dateTimeTools/CalendarPrinter.java
@@ -4,72 +4,63 @@ import pizzeria.dateTimeTools.discounts.HolidaySales;
 import pizzeria.dateTimeTools.discounts.SpecialWeeklyDiscounts;
 
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.temporal.ChronoField;
+import java.util.Arrays;
 
 public class CalendarPrinter {
 
-    static final String ANSI_RESET = "\u001B[0m";
-    static final String ANSI_RED = "\u001B[31m";
-    static final String ANSI_GREEN = "\u001B[32m";
-
-    static boolean isHoliday(LocalDate date) {
-        for (int i = 0; i < HolidaySales.values().length; i++) {
-            if (HolidaySales.values()[i].getDate().equals(date)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean isDiscountsToday(LocalDate date){
-        for (int i = 0; i < SpecialWeeklyDiscounts.values().length; i++) {
-            if (SpecialWeeklyDiscounts.values()[i].getDayOfWeek() == date.get(ChronoField.DAY_OF_WEEK) && !isHoliday(date)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-
     public static void printYearCalendar(int year) {
-        LocalDate start = LocalDate.of(year, 1, 1);
-        for (int i = 0; i < 12; i++) {
-            LocalDate date = start.plusMonths(i);
-            System.out.println("\n" + date.getMonth());
-            printMonth(date.getMonthValue(), year);
-            System.out.println();
-        }
-
+        Arrays.stream(Month.values())
+                .forEach(month -> {System.out.println("\n" + month);
+                    printMonth(month.getValue(), year);
+                    System.out.println();
+                });
     }
+
     private static void printMonth(int month, int year) {
         System.out.println("Mon   Tue   Wed   Thu   Fri   Sat   Sun");
         LocalDate currentDate = LocalDate.of(year, month, 1);
-        for (int i = 0; i < currentDate.lengthOfMonth(); i++) {
-            for (int j = 1; j <= 7 && i < currentDate.lengthOfMonth(); j++) {
-                if (currentDate.plusDays(i).get(ChronoField.DAY_OF_WEEK) == j) {
-                    printColoredDay(currentDate, i);
-                    if (currentDate.plusDays(i).get(ChronoField.DAY_OF_MONTH)/10 == 0) {
-                        System.out.print(" ");
-                    }
-                    System.out.print("    ");
-                    i++;
+        for (int dayOfMonth = 0; dayOfMonth < currentDate.lengthOfMonth(); dayOfMonth++) {
+            for (int dayOfWeek = 1; dayOfWeek <= 7 && dayOfMonth < currentDate.lengthOfMonth(); dayOfWeek++) {
+                if (currentDate.plusDays(dayOfMonth).get(ChronoField.DAY_OF_WEEK) == dayOfWeek) {
+                    printColoredDay(currentDate, dayOfMonth);
+                    printSpaces(currentDate, dayOfMonth);
+                    dayOfMonth++;
                 } else {
                     System.out.print("      ");
                 }
             }
             System.out.println();
-           i--;
+            dayOfMonth--;
         }
     }
 
     private static void printColoredDay(LocalDate date, int i){
         if (isHoliday(date.plusDays(i))) {
-            System.out.print(ANSI_RED + date.plusDays(i).get(ChronoField.DAY_OF_MONTH) + ANSI_RESET);
+            System.out.print(Colors.RED.getCode() + date.plusDays(i).get(ChronoField.DAY_OF_MONTH) + Colors.RESET.getCode());
         } else if (isDiscountsToday(date.plusDays(i))) {
-            System.out.print(ANSI_GREEN + date.plusDays(i).get(ChronoField.DAY_OF_MONTH) + ANSI_RESET);
+            System.out.print(Colors.GREEN.getCode() + date.plusDays(i).get(ChronoField.DAY_OF_MONTH) + Colors.RESET.getCode());
         } else {
             System.out.print(date.plusDays(i).get(ChronoField.DAY_OF_MONTH));
         }
+    }
+
+    private static void printSpaces(LocalDate currentDate, int i) {
+        if (currentDate.plusDays(i).get(ChronoField.DAY_OF_MONTH)/10 == 0) {
+            System.out.print(" ");
+        }
+        System.out.print("    ");
+    }
+
+    private static boolean isHoliday(LocalDate date) {
+        return Arrays.stream(HolidaySales.values()).anyMatch(sale -> sale.getDate().equals(date));
+    }
+
+    private static boolean isDiscountsToday(LocalDate date){
+        return Arrays.stream(SpecialWeeklyDiscounts.values()).anyMatch(discount ->
+            discount.getDayOfWeek() == date.get(ChronoField.DAY_OF_WEEK) && !isHoliday(date)
+        );
     }
 
 }
---
+++ src/pizzeria/dateTimeTools/Colors.java
@@ -0,0 +1,13 @@
+package pizzeria.dateTimeTools;
+
+public enum Colors {
+    RED("\u001B[31m"), GREEN("\u001B[32m"), RESET("\u001B[0m");
+    private String code;
+    Colors(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
