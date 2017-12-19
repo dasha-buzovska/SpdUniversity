0d90a86132f8040ddb2999029b3e712d89c7950b
Daryna Buzovska <buzovskadaryna@gmail.com>
2017-12-19 12:04:13 +0200

    HW11 - WIP: download and write in txt file

--- src/concurrency/Controller.java
+++ src/concurrency/Controller.java
@@ -1,22 +1,25 @@
 package concurrency;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 //Написать файлЕнтри
 //Написать ридер и райтер для файлов
-
 //написать довнлоадер
-//написать к нему промерки
+
+//написать к нему проверки
 //наисать средпул
 //оформить контролер
 //запустить
 
 public class Controller {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         List<FileEntry> list = FileManager.parseFileEntry();
-        list.stream().map(FileEntry::toString).forEach(System.out::println);
+        File file = new File(FileManager.DIRECTORY + list.get(0).getId() + ".txt");
+        Downloader.download(list.get(0).getUrl(), file);
         //питаємо у користувача число н, менше кількості ссилок
         //check the number and rewrite if bad
         //ExecutorService executor = Executors.newFixedThreadPool(5);
--- src/concurrency/Downloader.java
+++ src/concurrency/Downloader.java
@@ -1,7 +1,20 @@
 package concurrency;
 
-public class Downloader {
-    public static void download() {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 
+public class Downloader {
+    public static void download(String urlString, File destination) throws IOException {
+        URL website = new URL(urlString);
+        try(ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+                FileOutputStream fos = new FileOutputStream(destination)) {
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
